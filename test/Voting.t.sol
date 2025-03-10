09b7a3e77649df6d7b57d4e3d45815895868802f
hudsonhrh <hudsonhrh7@gmail.com>
2025-03-10 15:18:14 -0500

    refactor: voting test now uses deployment setup

--- test/Voting.t.sol
+++ test/Voting.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "forge-std/Test.sol";
+import "./DeploymentSetup.t.sol";
 import "@hamza-escrow/security/HatsSecurityContext.sol";
 import "../src/tokens/GovernanceToken.sol";
 import "../src/HamzaGovernor.sol";
@@ -11,39 +11,21 @@ import "@openzeppelin/contracts/governance/TimelockController.sol";
 import { HamzaGovernor } from "../src/HamzaGovernor.sol";
 import { Hats } from "@hats-protocol/Hats.sol";
 
-//TODO: inherit from DeploymentSetup
-contract VotingTest is Test {
-    HatsSecurityContext securityContext;
-    GovernanceToken govToken;
-    HamzaGovernor governor;
-    TestToken lootToken;
-    SystemSettings systemSettings;
-    TimelockController timelock;
+contract VotingTest is DeploymentSetup {
     string proposalDescription = "Test Proposal";
 
-    address admin;
     address[] voters;
     address[] targets;
     uint256[] values;
     bytes[] calldatas;
 
-    uint256 public adminHatId;
 
-    enum ProposalState {
-        Pending,
-        Active,
-        Canceled,
-        Defeated,
-        Succeeded,
-        Queued,
-        Expired,
-        Executed
-    }
 
-    function setUp() public {
-        address[] memory empty;
+    function setUp() public override {
+        // Call DeploymentSetup's setUp function first
+        super.setUp();
 
-        admin = address(0x12);
+        // Initialize voters
         voters.push(address(0x13));
         voters.push(address(0x14));
         voters.push(address(0x15));
@@ -54,198 +36,152 @@ contract VotingTest is Test {
         voters.push(address(0x20));
         voters.push(address(0x21));
 
-        //security context
-        securityContext = createHatsSecurityContext();
+        // We'll use existing lootToken from DeploymentSetup (script.hamzaToken())
+        // We'll use existing govToken from DeploymentSetup
+        
+        // Use the existing securityContext, lootToken, and govToken from DeploymentSetup
+        HatsSecurityContext securityContextLocal = HatsSecurityContext(hatsCtx);
+        TestToken lootTokenLocal = TestToken(lootToken);
+        GovernanceToken govTokenLocal = GovernanceToken(govToken);
 
-        //create tokens & mint
-        lootToken = new TestToken("LOOT", "LOOT");
-        govToken = new GovernanceToken(securityContext, lootToken, "Hamg", "HAMG");
-
-        //mint tokens 
+        // Mint loot tokens to voters
         for(uint256 n=0; n<voters.length; n++) {
-            lootToken.mint(voters[n], 100);
+            vm.startPrank(baal);
+            lootTokenLocal.mint(voters[n], 100);
+            vm.stopPrank();
         }
 
-        vm.startPrank(admin);
-        
-        // deploy main contracts
-        timelock = new TimelockController(1, empty, empty, admin);
-        governor = new HamzaGovernor(govToken, timelock);
-        systemSettings = new SystemSettings(securityContext, admin, 0);
+        // We'll use the existing governor and timelock from DeploymentSetup
 
-        //grant permissions for governor
-        timelock.grantRole(keccak256("EXECUTOR_ROLE"), address(governor));
-        timelock.grantRole(0xb09aa5aeb3702cfd50b6b62bc4532604938f21248a27a1d5ca736082b6819cc1, address(governor));
+        // Mint hats to voters
+        vm.startPrank(safe);
+        Hats hats = Hats(securityContextLocal.hats());
+        uint256 minterHatId = script.minterHatId();
 
-        // 1) Grab the Hats protocol reference & adminHatId from our security context
-        Hats hats = Hats(securityContext.hats()); 
-
-        // 2) Create new child-hats as needed for tests
-        uint256 daoHatId = hats.getNextId(adminHatId);
-        hats.createHat(
-            adminHatId,
-            "DAO Hat",   // optional name
-            2,          // maxSupply
-            address(1), // eligibility module (none in this simple test)
-            address(1), // toggle module (none in this simple test)
-            true,       // mutable
-            ""          // details
-        );
-        uint256 minterHatId = hats.getNextId(adminHatId);
-        hats.createHat(
-            adminHatId,
-            "Minter Hat",   // optional name
-            uint32(voters.length),      // maxSupply
-            address(1), // eligibility module (none in this simple test)
-            address(1), // toggle module (none in this simple test)
-            true,       // mutable
-            ""          // details
-        );
-
-        // 3) Assign roles to hats
-        securityContext.setRoleHat(Roles.DAO_ROLE, daoHatId);
-        securityContext.setRoleHat(Roles.MINTER_ROLE, minterHatId);
-
-        // 4) Mint the hats
-        hats.mintHat(daoHatId, address(timelock));
         for(uint8 n=0; n<voters.length; n++) {
             hats.mintHat(minterHatId, voters[n]);
         }
+        vm.stopPrank();
 
-        //prepare proposal data
-        targets.push(address(systemSettings));
+        // Prepare proposal data
+        targets.push(systemSettings);
         values.push(uint256(0));
         calldatas.push(abi.encodeWithSignature("setFeeBps(uint256)", 1));
 
-        vm.stopPrank();
-
+        // Have all voters deposit their tokens to get governance rights
         for(uint8 n=0; n<voters.length; n++) {
             vm.startPrank(voters[n]);
-            lootToken.approve(address(govToken), 100);
-            govToken.depositFor(voters[n], 100);
+            lootTokenLocal.approve(govToken, 100);
+            govTokenLocal.depositFor(voters[n], 100);
             vm.stopPrank();
         }
     }
 
-    function createHatsSecurityContext() internal returns (HatsSecurityContext) {
-        address hatsAddress = 0x3bc1A0Ad72417f2d411118085256fC53CBdDd137;
-        Hats hats = Hats(hatsAddress);
-        adminHatId = uint256(hats.lastTopHatId()) << 224;
-        // mint top-hat to admin
-        hats.mintTopHat(admin, "ipfs://bafkreih3vqseitn7pijlkl2jcawbjrhae3dfb2pakqtgd4epvxxfulwoqq","");
-        adminHatId = uint256(hats.lastTopHatId()) << 224;
-
-        securityContext = new HatsSecurityContext(hatsAddress, adminHatId);
-
-
-        return securityContext;
-    }
-
     function vote(address addr, uint256 proposal, uint8 support) internal {
         vm.startPrank(addr);
-        governor.castVote(proposal, support);
+        HamzaGovernor(governor).castVote(proposal, support);
         vm.stopPrank();
     }
     
     function testProposeVote() public {
-        //make a proposal
-        uint256 proposal = governor.propose(targets, values, calldatas, proposalDescription);
+        // Make a proposal
+        uint256 proposal = HamzaGovernor(governor).propose(targets, values, calldatas, proposalDescription);
 
-        //test proposal
+        // Test proposal
         assertGt(proposal, 0);
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Pending));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Pending));
         
-        //roll fwd
-        vm.roll(block.number +2);
+        // Roll forward
+        vm.roll(block.number + 2);
 
-        //state here should be active 
-        assertEq(uint256(governor.state(proposal)), uint(ProposalState.Active));
+        // State here should be active 
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint(ProposalState.Active));
     }
     
     function testVote() public {
-        uint256 proposal = governor.propose(targets, values, calldatas, proposalDescription);
-        vm.roll(block.number +2);
-        assertEq(uint256(governor.state(proposal)), uint(ProposalState.Active));
+        uint256 proposal = HamzaGovernor(governor).propose(targets, values, calldatas, proposalDescription);
+        vm.roll(block.number + 2);
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint(ProposalState.Active));
 
-        //let everyone vote for
+        // Let everyone vote for
         for(uint8 n=0; n<voters.length; n++) {
             vote(voters[n], proposal, 1);
         }
 
-        //roll forward
-        vm.roll(block.number +50401);
+        // Roll forward
+        vm.roll(block.number + 50401);
         vm.warp(block.timestamp + 50401);
 
-        //assert number of votes & checkpoints
+        // Assert number of votes & checkpoints
         for(uint8 n=0; n<voters.length; n++) {
-            assertEq(govToken.getVotes(voters[n]), 100);
+            assertEq(GovernanceToken(govToken).getVotes(voters[n]), 100);
         }
         for(uint8 n=0; n<voters.length; n++) {
-            assertEq(govToken.numCheckpoints(voters[n]), 1);
+            assertEq(GovernanceToken(govToken).numCheckpoints(voters[n]), 1);
         }
 
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Succeeded));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Succeeded));
     }
     
     function testUnanimousVoteDefeat() public {
-        assertEq(systemSettings.feeBps(), 0);
-        vm.roll(block.number +1);
+        assertEq(SystemSettings(systemSettings).feeBps(), initialFeeBps);
+        vm.roll(block.number + 1);
         
-        //create proposal
-        uint256 proposal = governor.propose(targets, values, calldatas, proposalDescription);
-        vm.roll(block.number +2);
-        assertEq(uint256(governor.state(proposal)), uint(ProposalState.Active));
+        // Create proposal
+        uint256 proposal = HamzaGovernor(governor).propose(targets, values, calldatas, proposalDescription);
+        vm.roll(block.number + 2);
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint(ProposalState.Active));
 
-        //let everyone vote agin
+        // Let everyone vote against
         for(uint8 n=0; n<voters.length; n++) {
             vote(voters[n], proposal, 0);
         }
 
-        //roll forward
-        vm.roll(block.number +50401);
+        // Roll forward
+        vm.roll(block.number + 50401);
 
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Defeated));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Defeated));
         vm.warp(block.timestamp + 999999);
 
-        assertEq(systemSettings.feeBps(), 0);
+        assertEq(SystemSettings(systemSettings).feeBps(), initialFeeBps);
     }
     
     function testNonUnanimousVoteDefeat() public {
-        assertEq(systemSettings.feeBps(), 0);
-        vm.roll(block.number +1);
+        assertEq(SystemSettings(systemSettings).feeBps(), initialFeeBps);
+        vm.roll(block.number + 1);
         
-        //create proposal
-        uint256 proposal = governor.propose(targets, values, calldatas, proposalDescription);
-        vm.roll(block.number +2);
-        assertEq(uint256(governor.state(proposal)), uint(ProposalState.Active));
+        // Create proposal
+        uint256 proposal = HamzaGovernor(governor).propose(targets, values, calldatas, proposalDescription);
+        vm.roll(block.number + 2);
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint(ProposalState.Active));
 
-        //let this guy vote for
+        // Let this guy vote for
         vote(voters[0], proposal, 1);
 
-        //let everyone else vote agin
+        // Let everyone else vote against
         for(uint8 n=1; n<voters.length; n++) {
             vote(voters[n], proposal, 0);
         }
 
-        //roll forward
-        vm.roll(block.number +50401);
+        // Roll forward
+        vm.roll(block.number + 50401);
 
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Defeated));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Defeated));
         vm.warp(block.timestamp + 999999);
 
-        assertEq(systemSettings.feeBps(), 0);
+        assertEq(SystemSettings(systemSettings).feeBps(), initialFeeBps);
     }
     
     function testNonUnanimousVoteExecute() public {
-        assertEq(systemSettings.feeBps(), 0);
-        vm.roll(block.number +1);
+        assertEq(SystemSettings(systemSettings).feeBps(), initialFeeBps);
+        vm.roll(block.number + 1);
         
-        //create proposal
-        uint256 proposal = governor.propose(targets, values, calldatas, proposalDescription);
-        vm.roll(block.number +2);
-        assertEq(uint256(governor.state(proposal)), uint(ProposalState.Active));
+        // Create proposal
+        uint256 proposal = HamzaGovernor(governor).propose(targets, values, calldatas, proposalDescription);
+        vm.roll(block.number + 2);
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint(ProposalState.Active));
 
-        //let everyone vote for
+        // Let everyone vote for or against in alternating pattern
         for(uint8 n=0; n<voters.length; n++) {
             if (n % 2 == 0)
                 vote(voters[n], proposal, 1);
@@ -253,88 +189,85 @@ contract VotingTest is Test {
                 vote(voters[n], proposal, 0);
         }
 
-        //roll forward
-        vm.roll(block.number +50401);
+        // Roll forward
+        vm.roll(block.number + 50401);
 
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Succeeded));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Succeeded));
         vm.warp(block.timestamp + 999999);
 
-        // queue 
-        governor.queue(targets, values, calldatas, keccak256(bytes(proposalDescription)));
+        // Queue 
+        HamzaGovernor(governor).queue(targets, values, calldatas, keccak256(bytes(proposalDescription)));
 
         vm.warp(block.timestamp + 2);
 
-        governor.execute(targets, values, calldatas, keccak256(bytes(proposalDescription)));
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Executed));
+        HamzaGovernor(governor).execute(targets, values, calldatas, keccak256(bytes(proposalDescription)));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Executed));
 
-        assertEq(systemSettings.feeBps(), 1);
+        assertEq(SystemSettings(systemSettings).feeBps(), 1);
     }
     
     function testProposalVote() public {
-        assertEq(systemSettings.feeBps(), 0);
-        vm.roll(block.number +1);
+        assertEq(SystemSettings(systemSettings).feeBps(), initialFeeBps);
+        vm.roll(block.number + 1);
         
-        //create proposal
-        uint256 proposal = governor.propose(targets, values, calldatas, proposalDescription);
-        vm.roll(block.number +2);
-        assertEq(uint256(governor.state(proposal)), uint(ProposalState.Active));
-
-        //vote(voters[0], proposal, 1);
+        // Create proposal
+        uint256 proposal = HamzaGovernor(governor).propose(targets, values, calldatas, proposalDescription);
+        vm.roll(block.number + 2);
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint(ProposalState.Active));
         
         for(uint8 n=0; n<voters.length; n++) {
             vote(voters[n], proposal, 1);
         }
-        //roll forward
-        vm.roll(block.number +50401);
+        // Roll forward
+        vm.roll(block.number + 50401);
 
-
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Succeeded));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Succeeded));
         vm.warp(block.timestamp + 999999);
 
-        // queue
-        governor.queue(targets, values, calldatas, keccak256(bytes(proposalDescription)));
+        // Queue
+        HamzaGovernor(governor).queue(targets, values, calldatas, keccak256(bytes(proposalDescription)));
 
         vm.warp(block.timestamp + 2);
 
-        governor.execute(targets, values, calldatas, keccak256(bytes(proposalDescription)));
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Executed));
+        HamzaGovernor(governor).execute(targets, values, calldatas, keccak256(bytes(proposalDescription)));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Executed));
 
-        assertEq(systemSettings.feeBps(), 1);
+        assertEq(SystemSettings(systemSettings).feeBps(), 1);
     }
     
     function testVoteQueue() public {
-        assertEq(systemSettings.feeBps(), 0);
-        vm.roll(block.number +1);
+        assertEq(SystemSettings(systemSettings).feeBps(), initialFeeBps);
+        vm.roll(block.number + 1);
         
-        //create proposal
-        uint256 proposal = governor.propose(targets, values, calldatas, proposalDescription);
-        vm.roll(block.number +2);
-        assertEq(uint256(governor.state(proposal)), uint(ProposalState.Active));
+        // Create proposal
+        uint256 proposal = HamzaGovernor(governor).propose(targets, values, calldatas, proposalDescription);
+        vm.roll(block.number + 2);
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint(ProposalState.Active));
 
-        //let everyone vote for
+        // Let everyone vote for
         for(uint8 n=0; n<voters.length; n++) {
             vote(voters[n], proposal, 1);
         }
 
-        //roll forward
-        vm.roll(block.number +50401);
+        // Roll forward
+        vm.roll(block.number + 50401);
 
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Succeeded));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Succeeded));
         vm.warp(block.timestamp + 999999);
 
-        //queue the proposal 
-        governor.queue(targets, values, calldatas, keccak256(bytes(proposalDescription)));
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Queued));
+        // Queue the proposal 
+        HamzaGovernor(governor).queue(targets, values, calldatas, keccak256(bytes(proposalDescription)));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Queued));
 
-        //and then wait 
-        vm.roll(block.number +50401);
+        // And then wait 
+        vm.roll(block.number + 50401);
         vm.warp(block.timestamp + 50401);
 
-        // and then execute
-        governor.execute(targets, values, calldatas, keccak256(bytes(proposalDescription)));
-        assertEq(uint256(governor.state(proposal)), uint256(ProposalState.Executed));
+        // And then execute
+        HamzaGovernor(governor).execute(targets, values, calldatas, keccak256(bytes(proposalDescription)));
+        assertEq(uint256(HamzaGovernor(governor).state(proposal)), uint256(ProposalState.Executed));
 
-        //the fee has not changed at this point
-        assertEq(systemSettings.feeBps(), 1);
+        // The fee has been changed at this point
+        assertEq(SystemSettings(systemSettings).feeBps(), 1);
     }
 }
