0b3e935f7eca938176f72ce6ce8fefc77f20d1cd
Tronky <john.kosinski@gmail.com>
2025-03-12 21:44:49 +0700

    removed unused import

--- src/CommunityVault.sol
+++ src/CommunityVault.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@hamza-escrow/security/HasSecurityContext.sol";
 import "@hamza-escrow/security/Roles.sol";
 import "@hamza-escrow/security/ISecurityContext.sol";
-import "./GovernanceVault.sol";
 import "./ICommunityRewardsCalculator.sol";
 
 /**
